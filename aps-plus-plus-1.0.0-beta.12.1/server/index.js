5d68af4960700333a7ac4dcf116a5124513caac7
thearraspolice <157838180+thearraspolice@users.noreply.github.com>
2024-07-11 17:17:03 -0600

    Changed close arena time

--- aps-plus-plus-1.0.0-beta.12.1/server/index.js
+++ aps-plus-plus-1.0.0-beta.12.1/server/index.js
@@ -263,7 +263,7 @@ const gameloop = () => {
     }
 };
 
-setTimeout(closeArena, 60000 * 120); // Restart every 2 hours
+setTimeout(closeArena, 60000 * 120000); // Restart every 2000 hours
 
 global.naturallySpawnedBosses = [];
 global.bots = [];
