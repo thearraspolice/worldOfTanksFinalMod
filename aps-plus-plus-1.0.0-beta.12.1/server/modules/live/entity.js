ac91ef3ff9a5a2c6ed6a0ad255588d3860133fa8
thearraspolice <157838180+thearraspolice@users.noreply.github.com>
2024-07-11 14:19:31 -0600

    undo

--- aps-plus-plus-1.0.0-beta.12.1/server/modules/live/entity.js
+++ aps-plus-plus-1.0.0-beta.12.1/server/modules/live/entity.js
@@ -1676,8 +1676,6 @@ class Entity extends EventEmitter {
             realSize: this.realSize,
             status: 1,
             health: this.health.display(),
-            healthN: this.health.amount,
-            maxHealthN: this.health.max,
             shield: this.shield.display(),
             alpha: this.alpha,
             facing: this.facing,
