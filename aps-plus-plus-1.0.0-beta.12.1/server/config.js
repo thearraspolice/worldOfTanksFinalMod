cf138ad60b5d1c2f0bde1bc01bdbeb0e7c14088c
thearraspolice <157838180+thearraspolice@users.noreply.github.com>
2024-08-04 06:05:22 -0600

    new map from toothless and grid tile size

--- aps-plus-plus-1.0.0-beta.12.1/server/config.js
+++ aps-plus-plus-1.0.0-beta.12.1/server/config.js
@@ -32,8 +32,8 @@ module.exports = {
     ROOM_SETUP: ['map_apspp_default'],
 
     // The dimensions of a single tile on the map.
-    TILE_WIDTH: 200,//400,
-    TILE_HEIGHT: 200,//400,
+    TILE_WIDTH: 350,//400,
+    TILE_HEIGHT: 350,//400,
 
 
 
